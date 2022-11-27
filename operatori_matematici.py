a0a4702801867ec66ee9ddeadb8c3940436327f9
Salvatore Capolupo <s.capolupo@gmail.com>
2022-11-27 23:24:29 +0100

    Create operatori_matematici.py

---
+++ operatori_matematici.py
@@ -0,0 +1,37 @@
+import random
+
+a = random.randint(1,5)
+b = random.randint(1,5)
+
+print ( "a =",a )
+print ( "b =",b )
+
+#somma
+print ( "a+b" )
+print ( a+b )
+print()
+
+#sottrazione
+print ( "a-b" )
+print ( a-b )
+print()
+
+#prodotto
+print ( "a*b" )
+print ( a*b )
+print()
+
+#a elevato a b
+print ( "a**b" )
+print ( a**b )
+print()
+
+#divisione 
+print ( "a/b (risultato decimale)" )
+print ( a/b )
+print()
+
+#divisione decimale
+print ( "a//b (risultato intero)" )
+print ( a//b )
+print()
