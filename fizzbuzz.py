f8532578f285e9d00732a3971aa934cec291cbad
Salvatore Capolupo <s.capolupo@gmail.com>
2022-11-27 22:52:54 +0100

    Create fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+n = int ( input ("n = ") )
+
+for fizzbuzz in range(1,100):
+    if fizzbuzz % 15 == 0:
+        print("FizzBuzz")
+        continue
+    elif fizzbuzz % 3 == 0:
+        print("Fizz")
+        continue
+    elif fizzbuzz % 5 == 0:
+        print("Buzz")
+        continue
+    print(fizzbuzz)
