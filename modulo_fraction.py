2c81c93bcd23c180399e56a596f1608682fbc7a1
Salvatore Capolupo <s.capolupo@gmail.com>
2022-11-28 08:03:07 +0100

    Create modulo_fraction.py

---
+++ modulo_fraction.py
@@ -0,0 +1,24 @@
+from fractions import Fraction
+
+print('3/7')
+print(Fraction(3, 7))
+print()
+
+print ("-16/10")
+print(Fraction(16, -10))
+print()
+
+print ("-8/5")
+print ( Fraction(-8, 5) )
+print()
+
+print ("123/1")
+print ( Fraction(123, 1) )
+print()
+
+print(' -3/7 ')
+print (Fraction(-3, 7) )
+print()
+
+print ("fraction 2.25")
+print ( Fraction(2.25) )
