8bb0cfbd7cd8dd739e041b499fe47f6a848ee22a
Salvatore Capolupo <s.capolupo@gmail.com>
2023-02-26 23:30:32 +0100

    Create espressioni.py

---
+++ espressioni.py
@@ -0,0 +1,92 @@
+# esercizio 1 
+x = 2          # a statement
+x = x + 2      # an expression
+print (x)
+
+# esercizio 2
+x = "2"          # a statement
+x = x + "2"      # an expression
+print (x)
+
+# esercizio 3
+x = 1.5          # a statement
+x = x - 2.5      # an expression
+print (x)
+
+# esercizio 4
+x = 1            # a statement
+x = x - 2.5      # an expression
+print (x)
+
+# esercizio 5
+x = 10          
+x = x / 3      
+print (x)
+
+# esercizio 6
+x = 10          
+x = x // 3      
+print (x)
+
+# esercizio 7
+x = 10          
+x = x ** 2      
+print (x)
+
+# esercizio 8
+x = 10          
+x = x % 2      
+print (x)
+
+# esercizio 9
+x = 10          
+x = x * 2      
+print (x)
+
+# esercizio 10
+x = 10          
+x = x ** 2      
+print (x)
+
+# esercizio 11
+a = True          
+b = False
+c = True
+print ( not a )
+print ( a and b)
+print ( a or b)
+print ( a and b or c)
+
+# esercizio 12
+a = 1          
+b = 2
+c = 3 
+print ( a + b - c * a // b)
+
+# esercizio 13
+a = 1          
+b = 2
+c = 3 
+print ( a // b - c * a - b)
+
+# esercizio 14
+a = 1          
+b = 2
+c = 3 
+print ( a ** b ** c)
+
+# esercizio 15
+a = 1          
+b = 1
+print ( a <= b )
+
+# esercizio 16 
+a = 1.1 + 2.2      
+valore_atteso = 3.3
+print ( a == valore_atteso ) # attenzione
+
+# esercizio 17
+a = 1.1 + 2.2   
+valore_atteso = 3.3
+tol = 0.00001
+print ( abs(a - valore_atteso) < tol ) # attenzione ...pure qui 
